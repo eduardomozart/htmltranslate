02450c8b07ccb64b772ece9efe2c54a05929d7cb
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-02-24 22:49:16 -0300

    Improved PHP 8 compat, bugfixes

--- translate.php
+++ translate.php
@@ -14,36 +14,42 @@ $dirlist = getFileList($rootdir);
 
 foreach($dirlist as $dir) {
     if(($dir['type']) !== "dir") { continue; };
+
+    $currdir = $dir['name'];
+    // $currdir = os_path_join(__DIR__, $dir['name']);
+
     // Limit Translation to the folder named "test"
+    // $currdir = 
     // if(strpos($dir['name'], "teste") == false) { continue; };
-    echo $dir['name'] . "\r\n";
-    // chdir(os_path_join(__DIR__, $dir['name']));
-    chdir($dir['name']);
+    
+    echo $currdir . "\r\n";
+
+    // chdir($currdir);
 
     // RPGM game
-    if (file_exists('Game.exe')) {
+    if (file_exists(os_path_join($currdir, 'Game.exe'))) {
       echo "RPGM game. Skipping.\r\n";
       continue;
     }
 
     // File to translate exists but isn't translated yet.
-    if (file_exists('data.txt')) {
+    if (file_exists(os_path_join($currdir, 'data.txt'))) {
       // echo "File " . os_path_join($dir['name'], 'data.txt') . " exists.\r\n";
-      if (!file_exists('data_trans.txt')) {
+      if (!file_exists(os_path_join($currdir, 'data_trans.txt'))) {
           echo "File to translate exists but isn't translated yet. Skipping.\r\n";
           continue;
         }
     }
 
-    $files = glob_recursive("*.htm*");
-    //var_dump($files);
+    $files = rsearch($currdir, "/.*\.html?$/");
+    // die(var_dump($files));
 
     // if (!file_exists('data_trans.txt')) {
-    if(!file_exists('data.txt')) {
-        $fp = fopen('data.txt', 'w'); // Open overwriting contents
+    if(!file_exists(os_path_join($currdir, 'data.txt'))) {
+        $fp = fopen(os_path_join($currdir, 'data.txt'), 'w'); // Open overwriting contents
         fwrite($fp, pack("CCC",0xef,0xbb,0xbf)); // Create file with "UTF-8 with BOM" encoding
-    } elseif (file_exists('data_trans.txt')) {
-        $tfp = explode("\n", file_get_contents('data_trans.txt'));
+    } elseif (file_exists(os_path_join($currdir, 'data_trans.txt'))) {
+        $tfp = explode("\n", file_get_contents(os_path_join($currdir, 'data_trans.txt')));
         // Remove lines that starts with hashtag (comments)
         $tfp = preg_replace(
             "/^\#(.*)$/m",
@@ -57,31 +63,33 @@ foreach($dirlist as $dir) {
     }
 
     foreach ($files as $file) {
+        echo $file . "\r\n";
         $pathinfo = pathinfo($file);
         $source_file = os_path_join($pathinfo['dirname'], $pathinfo['filename'] . '_orig.' . $pathinfo['extension']);
-        if (strpos($pathinfo['dirname'], 'flash') !== false) { /* echo "Directory contains 'flash'. Skipping.\r\n"; */ continue; }
-        if (substr($pathinfo['filename'], -3) == '_orig') { /* echo "Original file from already translated content. Skipping.\r\n"; */ continue; }
-        if (file_exists($source_file)) { /* echo "File " . $source_file . " already exists. Skipping.\r\n"; */ continue; }
+        if (strpos($pathinfo['dirname'], 'flash') !== false && !file_exists(os_path_join($currdir, 'data_trans.txt'))) { echo "Directory contains 'flash'. Skipping.\r\n"; continue; }
+        if (substr($pathinfo['filename'], -3) == '_orig') { echo "Original file from already translated content. Skipping.\r\n"; continue; }
+        if (file_exists($source_file)) { echo "File " . $source_file . " already exists. Skipping.\r\n"; continue; }
 
-        if (!file_exists('data_trans.txt')) {
-            echo "\r\nExtracting text to translate from " . os_path_join($dir['name'], $file) . "\r\n";
-            fwrite($fp, "# " . os_path_join($file) . "\r\n");
-        } else {
-          echo "\r\nTranslating " . os_path_join($dir['name'], $file) . "\r\n";
+        if (!file_exists(os_path_join($currdir, 'data_trans.txt'))) {
+            echo "\r\nExtracting text to translate from " . os_path_join($currdir, $file) . "\r\n";
+            fwrite($fp, "# " . $file . "\r\n");
         }
 
         //
         $html = file_get_contents($file);
         if (mb_detect_encoding($html, "SJIS-win, UTF-8") == "SJIS-win") {
             $htmlenconding = "SJIS-win";
-            $html = mb_convert_encoding($html, "UTF-8", "SJIS-win");
-            echo "Encoding: SJIS-win.\r\n";
+            // Directory do not contains "flash" on path
+            if(strpos($pathinfo['dirname'], 'flash') == false) {
+              $html = mb_convert_encoding($html, "UTF-8", "SJIS-win");
+            }
         } else {
-            echo "Encoding: UTF-8.\r\n";
+            $htmlenconding = "UTF-8";
         }
+        echo "Encoding: " . $htmlenconding . "\r\n";
         // echo $html;
 
-        libxml_use_internal_errors(true);
+        // libxml_use_internal_errors(true);
         $dom = new DomDocument();
         $dom->loadHTML($html);
         $xpath = new DOMXPath($dom);
@@ -90,7 +98,10 @@ foreach($dirlist as $dir) {
             not(ancestor::script) and
             not(normalize-space(.) = "")
         ]') as $text) {
-            /* if (trim(translateText('ja', 'en', utf8_decode($text->nodeValue)))) {
+            // Directory contains 'flash'. Skipping.
+            if(strpos($pathinfo['dirname'], 'flash') !== false) { continue; }
+
+            /* if (trim(translateText('ja', 'en', mb_convert_encoding($text->nodeValue, 'ISO-8859-1', 'UTF-8')))) {
                 $text->nodeValue = $output;
             } else {
                 echo "Google Translator limit probably exceeded.\r\n";
@@ -98,7 +109,7 @@ foreach($dirlist as $dir) {
             } */
 
             // Remove Tabs characters
-            $text->nodeValue = trim(str_replace('　', '', utf8_decode($text->nodeValue)));
+            $text->nodeValue = trim(str_replace('　', '', mb_convert_encoding($text->nodeValue, 'ISO-8859-1', 'UTF-8')));
             // data.txt is open, so we are generating the source translation file.
             if (@is_resource($fp)) {
                 //echo $text->nodeValue;
@@ -109,8 +120,10 @@ foreach($dirlist as $dir) {
                 if(trim($text->nodeValue)) {
                     fwrite($fp, $text->nodeValue . "\r\n");
                 }
-            } elseif (file_exists('data_trans.txt')) {
+            } elseif (file_exists(os_path_join($currdir, 'data_trans.txt'))) {
                 // Translate
+                echo "\r\nTranslating " . $file . "\r\n";
+
                 if (array_key_exists($counter, $tfp)) {
                     if(trim($text->nodeValue)) {
                         $text->nodeValue = $tfp[$counter];
@@ -128,7 +141,7 @@ foreach($dirlist as $dir) {
 
         // Add UTF-8 tag and detect if there's embed (SWF) objects.
         // If translating (both file exists so the HTML files are being translated)
-        if(file_exists('data.txt') && file_exists('data_trans.txt')) {
+        if(file_exists(os_path_join($currdir, 'data.txt')) && file_exists(os_path_join($currdir, 'data_trans.txt'))) {
             $head = $xpath->query('//head')->item(0);
 
             // Add Meta Charset
@@ -145,44 +158,45 @@ foreach($dirlist as $dir) {
 
             // $embed = $xpath->query('//embed');
             // echo $embed->length;
-			$dirsrc = os_path_join('..', 'ruffle');
-			if (file_exists($dirsrc)) {
-				foreach ($xpath->query('//embed') as $embed) {
-					$embedReplace = str_replace(strtolower($pathinfo['filename']), $pathinfo['filename'], utf8_decode($embed->getAttribute('src')));
-					// $embedReplace = mb_convert_encoding(utf8_decode($embedReplace), "SJIS-win", "ISO-8859-1");
+            $dirsrc = os_path_join($currdir, '..', 'ruffle');
+            // echo 'file_exists("' . $dirsrc . '"): ' . boolval(file_exists($dirsrc)) . "\r\n";
+            if (file_exists($dirsrc)) {
+              foreach ($xpath->query('//embed') as $embed) {
+                $embedReplace = str_replace(strtolower($pathinfo['filename']), $pathinfo['filename'], mb_convert_encoding($embed->getAttribute('src'), 'ISO-8859-1', 'UTF-8'));
+                // $embedReplace = mb_convert_encoding(mb_convert_encoding($embedReplace, 'ISO-8859-1', 'UTF-8'), "SJIS-win", "ISO-8859-1");
 
-					//echo "Original Embed: " . utf8_decode($embed->getAttribute('src')) . "\r\n";
-					//echo "Fixed Embed: " . $embedReplace . "\r\n";
-					//continue;
-					$embed->setAttribute('src',$embedReplace);
+                //echo "Original Embed: " . mb_convert_encoding($embed->getAttribute('src'), 'ISO-8859-1', 'UTF-8');
+                //echo "Fixed Embed: " . $embedReplace . "\r\n";
+                //continue;
+                $embed->setAttribute('src',$embedReplace);
 
-					// Will Copy Ruffle Files
-					
-					$dirtgt = os_path_join($pathinfo['dirname'], 'ruffle');
-					if(!file_exists($dirtgt)) {
-						// echo "Copy Ruffle:\r\ndirsrc = " . $dirsrc . "\r\ndirtgt = " . $dirtgt;
-						custom_copy($dirsrc, $dirtgt);
-					}
+                // Will Copy Ruffle Files
+                
+                $dirtgt = os_path_join($pathinfo['dirname'], 'ruffle');
+                if(!file_exists($dirtgt)) {
+                  // echo "Copy Ruffle:\r\ndirsrc = " . $dirsrc . "\r\ndirtgt = " . $dirtgt;
+                  custom_copy($dirsrc, $dirtgt);
+                }
 
-					// Add Ruffle
-					// https://github.com/ruffle-rs/ruffle/wiki/Using-Ruffle
-					// https://stackoverflow.com/questions/65465492/how-to-embed-ruffle-flash-player-emulator-into-html-file
-					// https://github.com/ruffle-rs/ruffle/discussions/2998
-					$domElement = $dom->createElement('script', 'window.RufflePlayer = {
-					  config: {
-						autoplay: "on",
-					  }
-					};');
-					$head->appendChild($domElement);
+                // Add Ruffle
+                // https://github.com/ruffle-rs/ruffle/wiki/Using-Ruffle
+                // https://stackoverflow.com/questions/65465492/how-to-embed-ruffle-flash-player-emulator-into-html-file
+                // https://github.com/ruffle-rs/ruffle/discussions/2998
+                $domElement = $dom->createElement('script', 'window.RufflePlayer = {
+                  config: {
+                  autoplay: "on",
+                  }
+                };');
+                $head->appendChild($domElement);
 
-					$domElement = $dom->createElement('script');
-					$domAttribute = $dom->createAttribute('src');
-					// Value for the created attribute
-					$domAttribute->value = './ruffle/ruffle.js';
-					$domElement->appendChild($domAttribute);
-					$head->appendChild($domElement);
-				}
-			}
+                $domElement = $dom->createElement('script');
+                $domAttribute = $dom->createAttribute('src');
+                // Value for the created attribute
+                $domAttribute->value = './ruffle/ruffle.js';
+                $domElement->appendChild($domAttribute);
+                $head->appendChild($domElement);
+              }
+            }
             // echo html_entity_decode($dom->saveHtml());
             // continue;
 
@@ -190,7 +204,9 @@ foreach($dirlist as $dir) {
             rename($file, $source_file);
 
             $hfp = fopen($file, 'w');
-            //fwrite($hfp, $dom->saveHtml((new \DOMXPath($dom))->query('/')->item(0)));
+            // fwrite($hfp, $dom->saveHTML($dom->documentElement));
+            // fwrite($hfp, $dom->saveHtml((new \DOMXPath($dom))->query('/')->item(0)));
+            // var_dump($dom->documentElement);
             fwrite($hfp, html_entity_decode($dom->saveHtml((new \DOMXPath($dom))->query('/')->item(0))));
             fclose($hfp);
         }
@@ -199,6 +215,10 @@ foreach($dirlist as $dir) {
     // Close data.txt handle if it's open (aka. generating the source translation file)
     if (@is_resource($fp)) {
         fclose($fp);
+    }
+
+    if(!filesize(os_path_join($currdir, 'data.txt'))) {
+      unlink(os_path_join($currdir, 'data.txt'));
     }
 }
 
@@ -211,13 +231,15 @@ function translateText($source, $target, $dtext) {
     return $output;
 }
 
-// Does not support flag GLOB_BRACE
-function glob_recursive($pattern, $flags = 0) {
-    $files = glob($pattern, $flags);
-    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-        $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
-    }
-    return $files;
+function rsearch($folder, $regPattern) {
+  $dir = new RecursiveDirectoryIterator($folder);
+  $ite = new RecursiveIteratorIterator($dir);
+  $files = new RegexIterator($ite, $regPattern, RegexIterator::GET_MATCH);
+  $fileList = array();
+  foreach($files as $file) {
+      $fileList = array_merge($fileList, $file);
+  }
+  return $fileList;
 }
 
 function os_path_join(...$parts) {
@@ -238,7 +260,7 @@ function getFileList($dir)
     $d = @dir($dir) or die("getFileList: Failed opening directory {$dir} for reading");
     while(FALSE !== ($entry = $d->read())) {
       // skip hidden files
-      if($entry{0} == ".") continue;
+      if($entry[0] == ".") continue;
       if(is_dir("{$dir}{$entry}")) {
         $retval[] = [
           'name' => "{$dir}{$entry}/",
